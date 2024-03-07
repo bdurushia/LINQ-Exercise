7f7f5c0e00ccb754cc810735885ae94bac72c23a
bdurushia <b.durushia@gmail.com>
2024-03-07 11:31:38 -0600

    Completed Exercise 2.

--- LINQ/Program.cs
+++ LINQ/Program.cs
@@ -4,7 +4,14 @@
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            var videoGames = new List<string>() 
+            { 
+                "Cyberpunk 2077", "Starfield", "Horizon: Zero Dawn", "Nier: Automata", "Hellblade: Senua's Sacrifice",
+                "Stray", "Shadow of the Tomb Raider", "Horizon: Forbidden West", "The Maser's Pupil", "Final Fantasy VII", "Control Ultimate Edition"
+            };
+
+            videoGames.OrderBy(name => name.Length).ToList().ForEach(name => Console.WriteLine(name));
+
         }
     }
 }
